bc925b56c0dd11ae0fddd756e1bcb3f345c466c5
kashivirus <kashivirus691@gmail.com>
2023-03-14 11:02:51 +0500

    asdasd

--- app/src/App.js
+++ app/src/App.js
@@ -24,7 +24,7 @@ class App extends React.Component {
     }
   }
   componentDidMount() {
-    axios.get("http://${this.localhost}:8080/api/")
+    axios.get("http://34.125.7.214:8080/api/")
       .then((res) => {
         this.setState({
           users: res.data,
@@ -39,7 +39,7 @@ class App extends React.Component {
     console.log(id)
     evenet.preventDefault();
     if (id === 0) {
-      axios.post("http://${this.localhost}:8080/api/", {
+      axios.post("http://34.125.7.214:8080/api/", {
         name: this.state.name,
         email: this.state.email,
         password: this.state.password
@@ -47,7 +47,7 @@ class App extends React.Component {
         this.componentDidMount();
       })
     } else {
-      axios.put("http://${this.localhost}:8080/api/", {
+      axios.put("http://34.125.7.214:8080/api/", {
         id: id,
         name: this.state.name,
         email: this.state.email,
@@ -58,13 +58,13 @@ class App extends React.Component {
     }
   }
   delete(id) {
-    axios.delete("http://${this.localhost}:8080/api/" + id)
+    axios.delete("http://34.125.7.214:8080/api/" + id)
       .then(() => {
         this.componentDidMount();
       })
   }
   edit(id) {
-    axios.get("http://${this.localhost}:8080/api/" + id)
+    axios.get("http://34.125.7.214:8080/api/" + id)
       .then((res) => {
         this.setState({
           id: res.data.id,
